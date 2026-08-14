283b4ba9d685b96b82a565604603be4a9588fc2e
Tyler Casey <tc@tylerbuilds.com>
2026-08-14 20:48:32 +0100

    Fix isolated builds and update Actions runtime

--- setup.py
+++ setup.py
@@ -1,39 +1,8 @@
 #!/usr/bin/env python3
+"""Compatibility shim for tooling that still invokes setup.py directly."""
 
-import sys, requests
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='drugwars',
-    version='1.2.1-post3',
-    author='Max Bridgland',
-    author_email='mabridgland@protonmail.com',
-    description='The 80s DOS game re-written in Python',
-    long_description=open('./README.md', 'r').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/M4cs/Drugwars',
-    packages=find_packages(),
-    install_requires=[
-        'terminaltables',
-        'requests'
-    ],
-    license='GNU General Public License v3 (GPLv3) (GPL)',
-    zip_safe=True,
-    entry_points={
-        'console_scripts':[
-            'drugwars = drugwars.__main__:main',
-        ],
-    },
-    classifiers=[  # Used by PyPI to classify the project and make it searchable
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: Microsoft',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Games/Entertainment :: Turn Based Strategy'
-    ]
-)
+
+if __name__ == "__main__":
+    setup()
